ff5a27bb447b901abd35b55a0ed7890a5966f9a3
Sebastian <sebastil@hotmail.com>
2023-06-19 22:17:17 +0800

    finished code

--- MatrixSolver.cs
+++ MatrixSolver.cs
@@ -1,5 +1,66 @@
 /// Author: Fernando Sebastian Lopez Ochoa
 
 class MatrixSolver {
+    public class queueElement {
+        public int x;
+        public int y;
+        public int dist;
 
+        public queueElement(int x, int y, int dist) {
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
+    }
+
+    static bool isValidCell(int x, int y, int n) {
+        return (x >= 0) && (x < n) && (y >= 0) && y < n;
+    }
+
+    static int []xMoves = {-1, 0, 1, -1, 1, -1, 0, 1};
+    static int []yMoves = {-1, -1, -1, 0, 0, 1, 1, 1};
+
+    /// Main function of the class. It uses a queue to perform breadth first search
+    /// The process will ignore those cells whose value is not 0, if it reaches dead
+    /// ends, the queue will be exhausted and will determine the corner is unreachable
+    /// Explanation of this algorithm is attached in the pdf
+    public static int Solve(int[,] matrixToSolve) {
+        int n = matrixToSolve.GetLength(0);
+        bool [,] visited = new bool[n, n];
+
+        // Return -1 directly if the matrix is unsolvable
+        if (matrixToSolve[0, 0] == 1 || matrixToSolve[n-1, n-1] == 1) {
+            return -1;
+        }
+
+        // 0,0 is the origin, so we set it up based on that location
+        visited[0, 0] = true;
+        Queue<queueElement> q = new Queue<queueElement>();
+        queueElement r = new queueElement(0, 0, 1);
+        q.Enqueue(r);
+
+        // BFS implementation, explained in the pdf
+        while (q.Count != 0) {
+            queueElement current = q.Peek();
+            int x = current.x;
+            int y = current.y;
+
+            if (x == n - 1 && y == n - 1) {
+                return current.dist;
+            }
+
+            q.Dequeue();
+
+            for (int i = 0; i < 8; i++) {
+                int newX = x + xMoves[i];
+                int newY = y + yMoves[i];
+                if (isValidCell(newX, newY, n) && matrixToSolve[newX, newY] == 0 && !visited[newX, newY]) {
+                    visited[newX, newY] = true;
+                    queueElement AdjacentCell = new queueElement(newX, newY, current.dist + 1);
+                    q.Enqueue(AdjacentCell);
+                }
+            }
+        }
+        return -1;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿/// Author: Fernando Sebastian Lopez Ochoa
+using System;
 
 /// Main class to run the program, located in the file 
 
 class Program {
     static public void Main() {
-        int[][] customInput = [[0, 1],[1, 0]]
-        
+        int[,] customInput = {{0, 0, 0},{1, 1, 0}, {1, 1, 0}};
+        int customOutput = MatrixSolver.Solve(customInput);
+        Console.WriteLine(customOutput.ToString());
     }
 }
